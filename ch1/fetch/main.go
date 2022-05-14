66eb0768517b1ec34cd7f44cd106efe34220799b
ambition <ambition.xx@qq.com>
2022-05-14 22:54:09 +0800

    练习1-7~1-9

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -9,25 +9,34 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
+	"strings"
+
+	// "io/ioutil"
 	"net/http"
 	"os"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		prefix := "http://"
+		if !strings.HasPrefix(url, prefix){
+			url = prefix + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		// b, err := ioutil.ReadAll(resp.Body)
+		b, err := io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		fmt.Printf("%v\n", b)
+		fmt.Printf("%v\n", resp.Status)
 	}
 }
 
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module gopl.io
 
 go 1.16
 
-require golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6
+require (
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.13 // indirect
+	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6
+	gorm.io/driver/sqlite v1.3.2 // indirect
+	gorm.io/gorm v1.23.5 // indirect
+)
