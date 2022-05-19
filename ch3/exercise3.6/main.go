ec8d988622603ab4c823a27d28a7de040944da72
ambition <ambition.xx@qq.com>
2022-05-20 00:02:24 +0800

    exercise3.5-3.9

--- ch3/exercise3.6/main.go
+++ ch3/exercise3.6/main.go
@@ -1,10 +1,3 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 61.
-//!+
-
-// Mandelbrot emits a PNG image of the Mandelbrot fractal.
 package main
 
 import (
@@ -23,6 +16,7 @@ func main() {
 		epsX                   = (xmax - xmin) / width
 		epsY                   = (ymax - ymin) / height
 	)
+
 	offX := []float64{-epsX, epsX}
 	offY := []float64{-epsY, epsY}
 
@@ -30,7 +24,6 @@ func main() {
 	for py := 0; py < height; py++ {
 		y := float64(py)/height*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
-
 			x := float64(px)/width*(xmax-xmin) + xmin
 			subPixels := make([]color.Color, 0)
 			for i := 0; i < 2; i++ {
@@ -40,14 +33,11 @@ func main() {
 				}
 			}
 			img.Set(px, py, avg(subPixels))
-			// z := complex(x, y)
-			// Image point (px, py) represents complex value z.
-			// img.Set(px, py, mandelbrot(z))
 		}
 	}
-	png.Encode(os.Stdout, img) // NOTE: ignoring errors
-
-	f, err := os.Create("image.png")
+	png.Encode(os.Stdout, img)
+	
+	f, err := os.Create("3.6image.png")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -70,7 +60,6 @@ func mandelbrot(z complex128) color.Color {
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			// return color.Gray{255 - contrast*n}
 			return getColor(n)
 		}
 	}
@@ -109,38 +98,4 @@ func avg(colors []color.Color) color.Color {
 		a += uint16(a_ / uint32(n))
 	}
 	return color.RGBA64{r, g, b, a}
-}
-//!-
-
-// Some other interesting functions:
-
-func acos(z complex128) color.Color {
-	v := cmplx.Acos(z)
-	blue := uint8(real(v)*128) + 127
-	red := uint8(imag(v)*128) + 127
-	return color.YCbCr{192, blue, red}
-}
-
-func sqrt(z complex128) color.Color {
-	v := cmplx.Sqrt(z)
-	blue := uint8(real(v)*128) + 127
-	red := uint8(imag(v)*128) + 127
-	return color.YCbCr{128, blue, red}
-}
-
-// f(x) = x^4 - 1
-//
-// z' = z - f(z)/f'(z)
-//    = z - (z^4 - 1) / (4 * z^3)
-//    = z - (z - 1/z^3) / 4
-func newton(z complex128) color.Color {
-	const iterations = 37
-	const contrast = 7
-	for i := uint8(0); i < iterations; i++ {
-		z -= (z - 1/(z*z*z)) / 4
-		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
-			return color.Gray{255 - contrast*i}
-		}
-	}
-	return color.Black
 }
